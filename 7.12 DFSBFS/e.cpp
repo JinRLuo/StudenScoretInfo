c5bf517547f0d87ba747c5eca4694461a860878d
JinRLuo <997331539@qq.com>
2019-07-15 14:19:42 +0800

    dfs/bfs

--- 7.12 DFSBFS/e.cpp
+++ 7.12 DFSBFS/e.cpp
@@ -8,15 +8,15 @@ struct node{
 };
 
 int n,m,k,x1,y1,x2,y2,flag;
-char c[15][15];
-int v[15][15];
-int t[15][15];
+char c[105][105];
+int v[105][105];
+int t[105][105];
 queue<node> q;
-int dx[4]={1,-1,0,0};
-int dy[4]={0,0,1,-1};
+int dx[4]={-1,1,0,0};
+int dy[4]={0,0,-1,1};
 
 int check(int x,int y){
-	if(x<1||x>n||y<1||y>m||v[x][y]==1||c[x][y]=='*')
+	if(x<1||x>m||y<1||y>n||c[x][y]=='*')
 		return 0;
 	return 1;
 }
@@ -25,28 +25,28 @@ void bfs(){
 	node n1,n2;
 	int nx,ny;
 	while(!q.empty()){
-		//cout << 1 << endl;
 		n1=q.front();
 		q.pop();
-		nx=n1.x;
-		ny=n1.y;
-		if(t[nx][ny]>k)
+		if(t[n1.x][n1.y]>k)
 			break;
-		if(nx==x2&&ny==y2&&t[nx][ny]<=k){
+		if(n1.x==x2&&n1.y==y2&&t[n1.x][n1.y]<=k){
 			flag=1;
 			return;
 		}
+		if(t[n1.x][n1.y]==k)
+			continue;
+		//n2=n1;
 		for(int i=0;i<4;i++){
-			nx+=dx[i];
-			ny+=dy[i];
-			while(check(nx,ny)){
-				t[nx][ny]=t[n1.x][n1.y]+1;
-				n2.x=nx;
-				n2.y=ny;
-				v[nx][ny]=1;
-				q.push(n2);
-				nx+=dx[i];
-				ny+=dy[i];
+			n2.x=n1.x+dx[i];
+			n2.y=n1.y+dy[i];
+			while(check(n2.x,n2.y)){
+				if(v[n2.x][n2.y]==0){
+					t[n2.x][n2.y]=t[n1.x][n1.y]+1;
+					v[n2.x][n2.y]=1;
+					q.push(n2);
+				}
+				n2.x+=dx[i];
+				n2.y+=dy[i];
 			}
 		}
 	}
@@ -59,10 +59,11 @@ int main(){
 	while(T--){
 		flag=0;
 		cin >> m >> n;
+		while(!q.empty()) q.pop();
 		for(int i=1;i<=m;i++)
 			for(int j=1;j<=n;j++)
 				cin >> c[i][j];
-		cin >> k >> x1 >> y1 >> x2 >> y2;
+		cin >> k >> y1 >> x1 >> y2 >> x2;
 		memset(v,0,sizeof(v));
 		memset(t,0x3f,sizeof(t));
 		node n1;
@@ -73,12 +74,12 @@ int main(){
 		q.push(n1);
 		bfs();
 		
-        for(int i=1;i<=m;i++){
-           for(int j=1;j<=n;j++)
-               cout<<t[i][j]<<" ";
-           cout<<endl;
-        }
-        cout<<k<<endl;
+//        for(int i=1;i<=m;i++){
+//           for(int j=1;j<=n;j++)
+//               cout<<t[i][j]<<" ";
+//           cout<<endl;
+//        }
+//        cout<<k<<endl;
          
 		if(flag==1)
 			cout << "yes" << endl;
