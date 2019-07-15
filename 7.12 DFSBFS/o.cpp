f9a28d77f940e66f6e48c87a49c7685fe2c0d87a
JinRLuo <997331539@qq.com>
2019-07-15 20:53:45 +0800

    dfs/bfs

--- 7.12 DFSBFS/o.cpp
+++ 7.12 DFSBFS/o.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <cstring>
+#define maxa 10000
 using namespace std;
 
 struct node{
@@ -17,23 +18,28 @@ int dx[4]={1,-1,0,0};
 int dy[4]={0,0,1,-1};
 
 int check(int x,int y){
-	if(x<0||x>=n||y<0||y>=m||v[x][y]==1)
+	if(x<0||x>=n||y<0||y>=m||v[x][y]==1||s[x][y]=='.')
 		return 0;
 	return 1;
 }
 
 int bfs(node n1,node n2){
+	int res;
 	memset(v,0,sizeof(v));
 	while(!q.empty()) q.pop();
 	v[n1.x][n1.y]=1;
+	v[n2.x][n2.y]=1;
 	n1.step=0;
+	n2.step=0;
 	q.push(n1);
+	q.push(n2);
 	node n3;
 	while(!q.empty()){
 		n2=q.front();
 		q.pop();
+		//cout << n2.x << " " << n2.y << " " << n2.step << endl;
 		if(q.empty()&&n2.step!=0)
-			return n2.step;
+			res=n2.step;
 		for(int i=0;i<4;i++){
 			n3.x=n2.x+dx[i];
 			n3.y=n2.y+dy[i];
@@ -44,14 +50,20 @@ int bfs(node n1,node n2){
 			}
 		}
 	}
-	return 0;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			if(s[i][j]=='#'&&v[i][j]==0)
+				return maxa;
+		}
+	}
+	return res;
 }
 
 int main(){
 	int T,cs=1,sum,ans,mina;
 	cin>>T;
 	while(T--){
-		mina=10000;
+		mina=maxa;
 		sum=0;
 		cin >> n >> m;
 		for(int i=0;i<n;i++){
@@ -66,12 +78,17 @@ int main(){
 		}
 		for(int i=0;i<sum;i++){
 			for(int j=0;j<sum;j++){
+//				cout << "i: " << nd[i].x << " " << nd[i].y << endl;
+//				cout << "j: " << nd[j].x << " " << nd[j].y << endl;
 				ans=bfs(nd[i],nd[j]);
-				cout << ans << endl;
+				//cout << ans << endl;
 				mina=mina>ans?ans:mina;
 			}
 		}
-		cout << "Case " << cs << ": " << mina << endl;
+		if(mina==maxa)
+			cout << "Case " << cs << ": " << -1 << endl;
+		else
+			cout << "Case " << cs << ": " << mina << endl;
 		cs++;
 	}
 	
